0d4c2b746657d9c16362497d7fc9f3d6bbdc72cd
Louis Gabriel <louis.gabriel@orange.fr>
2019-08-17 07:24:17 +0200

    Example of 'early return' error handling

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func readFile(filename string) (string, error) {
+	dat, err := ioutil.ReadFile(filename)
+	// error handling of "early return" style (idiomatic style in Go)
+	if err != nil {
+		return "", err
+	}
+	if len(dat) == 0 {
+		//return "", errors.New("Empty content")
+		return "", fmt.Errorf("Empty content (filename=%v)", filename)
+	}
+	// code if no error
+	return string(dat), nil // err = nil means err has not been defined
+}
 
 func main() {
-	fmt.Println("Hello Gophers")
+	dat, err := readFile("test.txt")
+	// "early return"
+	if err != nil {
+		fmt.Printf("Error while reading file: %v\n", err)
+		return
+	}
+	// code if no error
+	fmt.Println("File content:")
+	fmt.Println(dat)
 }
